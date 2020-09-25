350fa22f5ebb8b57f37400036e5368c126bc7278
DBenson1017 <devinbenson@gmail.com>
2020-09-25 08:58:26 -0400

    initial commit

---
+++ src/components/User.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class User extends React.Component {
+
+    render(){
+        return (
+            <h3>User component test</h3>
+
+        )
+    }
+}
+
+
+export default User
---
+++ src/containers/MainContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import User from '../components/User'
+
+class MainContainer extends React.Component {
+
+    render () {
+        return(
+            <User />
+        )
+    }
+}
+
+export default MainContainer 
