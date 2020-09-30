b94e82f5c19e80abfe1313691f57b5f884bda54a
cathjime <xcatherine.jimenez@gmail.com>
2020-09-30 18:54:08 -0400

    login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
+import { Checkbox} from 'semantic-ui-react'
 
 class Login extends React.Component {
 
@@ -23,16 +25,39 @@ class Login extends React.Component {
     render () {
         return(
             <>
-            <h3>Log into your account</h3>
-          <form onSubmit={this.submitHandler}>
-            <input onChange={this.changeHandler} name='username' placeholder='username' type='text' value={this.state.username} /> <br></br>
-            <input onChange={this.changeHandler} name='password' placeholder='password' type='text' value={this.state.password} /> <br></br>
-            <input type='submit' value='login' />
-          </form>
-          </>
+                <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+                    <Grid.Column style={{ maxWidth: 450 }}>
+                    <Header as='h2' color='white' textAlign='center'>
+                        Log-in to your account
+                    </Header>
+                    <Form>
+                    <form onSubmit={this.submitHandler}>
+                        <Form.Field>
+                            <input onChange={this.changeHandler} name='username' placeholder='username' type='text' value={this.state.username} /> 
+                        </Form.Field>
+                        <Form.Field>
+                            <input onChange={this.changeHandler} name='password' placeholder='password' type='text' value={this.state.password} /> 
+                        </Form.Field>
+                        <Button> 
+                            <input type='submit' value='login' />
+                        </Button>
+                        </form>
+                        </Form>
+                    </Grid.Column>
+                </Grid>
+            </>
         )
     }
 
 }
 
 export default Login 
+
+
+
+
+
+
+  
+
+
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,14 +1,5 @@
 import React from 'react';
-import {
-  Button,
-  Divider,
-  Grid,
-  Header,
-  Icon,
-  Search,
-  Segment,
-} from 'semantic-ui-react'
-
+import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
 
 
 class Signup extends React.Component {
@@ -37,6 +28,7 @@ class Signup extends React.Component {
         return(
             <>
             <h3>Sign up for an account here!</h3>
+            <Segment raised>
             <form onSubmit={this.submitHandler}>
                 <input onChange={this.changeHandler} name='first_name' placeholder='First Name' type='text' value={this.state.first_name} /> <br></br>
                 <input onChange={this.changeHandler} name='last_name' placeholder='Last Name' type='text' value={this.state.last_name} /><br></br>
@@ -46,9 +38,16 @@ class Signup extends React.Component {
                 <input onChange={this.changeHandler} name='zip' placeholder='zip code' type='text' value={this.state.zip}/><br></br>
                 <input type='submit' value='Submit' />
             </form>
+            </Segment>
           </>
         )
     }
 
 }
 export default Signup 
+
+
+
+
+
+
