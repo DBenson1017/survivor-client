e66f6209d69a8127e3aa0c6558f5da833752a404
cathjime <xcatherine.jimenez@gmail.com>
2020-09-30 14:25:48 -0400

    installed semantic and successfully tested

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'semantic-ui-css/semantic.min.css'
+import { Button } from 'semantic-ui-react'
 
 ReactDOM.render(
 
@@ -17,3 +19,11 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+
+
+
+
+
+// export default ButtonExampleButton
